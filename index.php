2d658560403984e0eceb50ad5878f7385c4be3d4
AlexL <chuiflame@gmail.com>
2024-03-22 09:48:49 +0200

    classes in php

--- index.php
+++ index.php
@@ -1,50 +1,68 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        function say_hello()
-        {
-            echo 'hello</br>';
-            echo 'lalala</br>';
-            $name = 'Alex</br>';
-            echo $name;
-        }
-        
-        say_hello();
+<?php
 
-        function goodbye(string $name)
-        {
-            echo 'До свидания, ' . $name;
-        }
-        
-        goodbye('Alex');
-        
-        function date_of_birth(int $date)
-        {
-            echo $date;
-        }
-        
-        function passport(string $surname, string $first_name, string $last_name, string $gender, string $country_of_birth, int $serial_number)
-        {
-            $passport = [
-                'surname' => $surname,
-                'first_name' => $first_name,
-                'last_name' => $last_name,
-                'gender' => $gender,
-                'country_of_birth' => $country_of_birth,
-                'serial_number' => $serial_number
-            ];
-            var_dump($passport);
-        }
-        
-        passport('Spark', 'John', 'Dmitrievich', 'man', 'USA', 4015137067);
-    ?>
-</body>
-</html>
+class Person
+{
+    public $name = 'Alex';
+    public $age = '25';
+    public $isMarried = false;
+
+    public function sayHello()
+    {
+        echo 'Hello my friends!</br>';
+    }
+}
+
+$person = new Person();
+
+$person->sayHello();
+
+class Course
+{
+    public string $course = 'PHP DEVELOPER';
+    public string $description = 'Курс "PHP Developer" предназначен для тех, кто стремится стать квалифицированным PHP-разработчиком. ';
+    public string $teacher = 'Leo';
+    public array $students =
+        [
+            'Пьер' => [
+                'first_name' => 'Пьер',
+                'last_name' => 'Безухов',
+                'age' => '20'
+            ],
+            'Наташа' => [
+                'first_name' => 'Наташа',
+                'last_name' => 'Ростова',
+                'age' => '17'
+            ],
+            'Андрей' => [
+                'first_name' => 'Андрей',
+                'last_name' => 'Болконский',
+                'age' => '25'
+            ],
+            'Анна' => [
+                'first_name' => 'Анна',
+                'last_name' => 'Шерер',
+                'age' => '50'
+            ],
+            'Долохов' => [
+                'first_name' => 'Долохов',
+                'age' => '30'
+            ],
+            'Марья' => [
+                'first_name' => 'Марья',
+                'last_name' => 'Болконская',
+                'age' => '20'
+            ]
+        ];
+
+}
+
+$course = new Course;
+
+echo $course->description . '</br>';
+
+foreach ($course->students as $key => $student) {
+    echo 'Student: ' . $key . '</br>';
+    foreach ($student as $k => $v) {
+        echo $k . ': ' . $v . '</br>' . 'Направление: ' . $course->course . '</br>';
+    }
+}
