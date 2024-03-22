dae6ec607e07481a94cb580ee4e0f94eb5186fd1
AlexL <chuiflame@gmail.com>
2024-03-22 11:31:07 +0200

    get, set

--- index.php
+++ index.php
@@ -6,6 +6,18 @@ class Person
     public $age = '25';
     public $isMarried = false;
 
+    public function setName($name) {
+        $this->name = $name;
+    }
+    public function setAge($age) {
+        $this->age = $age;
+    }
+    public function getName() {
+       return $this->name;
+    }
+    public function getAge() {
+       return $this->age;
+    }
     public function sayHello()
     {
         echo 'Hello my friends!</br>';
@@ -13,56 +25,92 @@ class Person
 }
 
 $person = new Person();
+$person2 = new Person();
+$person3 = new Person();
 
-$person->sayHello();
 
-class Course
-{
-    public string $course = 'PHP DEVELOPER';
-    public string $description = 'Курс "PHP Developer" предназначен для тех, кто стремится стать квалифицированным PHP-разработчиком. ';
-    public string $teacher = 'Leo';
-    public array $students =
-        [
-            'Пьер' => [
-                'first_name' => 'Пьер',
-                'last_name' => 'Безухов',
-                'age' => '20'
-            ],
-            'Наташа' => [
-                'first_name' => 'Наташа',
-                'last_name' => 'Ростова',
-                'age' => '17'
-            ],
-            'Андрей' => [
-                'first_name' => 'Андрей',
-                'last_name' => 'Болконский',
-                'age' => '25'
-            ],
-            'Анна' => [
-                'first_name' => 'Анна',
-                'last_name' => 'Шерер',
-                'age' => '50'
-            ],
-            'Долохов' => [
-                'first_name' => 'Долохов',
-                'age' => '30'
-            ],
-            'Марья' => [
-                'first_name' => 'Марья',
-                'last_name' => 'Болконская',
-                'age' => '20'
-            ]
-        ];
+echo $person->name . '</br>';
+echo $person2->name . '</br>';
+echo $person3->name . '</br>';
 
-}
+$person2->setName('Leo');
+$person3->setName('Li');
 
-$course = new Course;
+echo '</br>';
+echo $person->name . '</br>';
+echo $person2->name . '</br>';
+echo $person3->name . '</br>';
 
-echo $course->description . '</br>';
+echo '</br>';
+echo $person->age . '</br>';
+echo $person2->age . '</br>';
+echo $person3->age . '</br>';
 
-foreach ($course->students as $key => $student) {
-    echo 'Student: ' . $key . '</br>';
-    foreach ($student as $k => $v) {
-        echo $k . ': ' . $v . '</br>' . 'Направление: ' . $course->course . '</br>';
+$person2->setAge(29);
+$person3->setAge(97);
+
+echo '</br>';
+echo $person->age . '</br>';
+echo $person2->age . '</br>';
+echo $person3->age . '</br>';
+
+echo '</br>';
+echo $person->getName() . '</br>';
+echo $person2->getName() . '</br>';
+echo $person3->getName() . '</br>';
+
+echo '</br>';
+echo $person->getAge() . '</br>';
+echo $person2->getAge() . '</br>';
+echo $person3->getAge() . '</br>';
+
+class Pet {
+    public $name = 'Willie';
+    public $age = 3;
+    public $type = 'dog';
+    
+    public function setName($name) {
+        $this->name = $name;
+    }
+    public function setAge($age) {
+        $this->age = $age;
+    }
+    public function setType($type) {
+        $this->type = $type;
+    }
+    public function getName() {
+        return $this->name;
+    }
+    public function getAge() {
+        return $this->age;
+    }
+    public function getType() {
+        return $this->type;
     }
 }
+
+$pet = new Pet();
+$pet2 = new Pet();
+$pet3 = new Pet();
+
+echo '</br>';
+echo $pet->getName() . '</br>';
+echo $pet->getAge() . '</br>';
+echo $pet->getType() . '</br>';
+
+echo '</br>';
+echo $pet2->setName('Ku') . '</br>';
+echo $pet2->setAge(12) . '</br>';
+echo $pet2->setType('Rat') . '</br>';
+echo $pet2->getName() . '</br>';
+echo $pet2->getAge() . '</br>';
+echo $pet2->getType() . '</br>';
+
+echo '</br>';
+echo $pet3->setName('Storm') . '</br>';
+echo $pet3->setAge(5) . '</br>';
+echo $pet3->setType('Cat') . '</br>';
+echo $pet3->getName() . '</br>';
+echo $pet3->getAge() . '</br>';
+echo $pet3->getType() . '</br>';
+
